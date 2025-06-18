c42e8dacb533627bb6feff00515842d52e73dd4a
EatenCaesar-ZA <irshadmoola@gmail.com>
2025-06-18 21:54:07 +0200

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,34 @@
+#Operators
+#addition(+)
+#subtraction(-)
+#multiplication(*)
+#division(/)
+#exponentiation(**)
+#modulus(%)
+
+x = 10
+y = 5
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x ** y)
+print(x % y)
+
+x = 10
+x += 5  # Increment x by 5
+print(x)  # Output: 15
+
+x -= 3  # Decrement x by 3
+print(x)  # Output: 12
+
+x *= 2  # Multiply x by 2
+print(x)  # Output: 24
+
+x /= 4  # Divide x by 4
+print(x)  # Output: 6.0
+
+# place an opererator infront of an = to assign a value to a variable
+#x **= 2  # Raise x to the power of 2
+         
