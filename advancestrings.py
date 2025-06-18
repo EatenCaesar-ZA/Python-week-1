f616ed1f427638cc36d96c9f4975356511042047
EatenCaesar-ZA <irshadmoola@gmail.com>
2025-06-18 13:22:01 +0200

    Phyton lessons

---
+++ advancestrings.py
@@ -0,0 +1,10 @@
+#Advanced Strings//
+
+message = " Hello, World! "
+
+print(message.strip()) # Removes leading and trailing whitespace
+print(message.lower()) # Converts to lowercase
+print(len(message)) # Gets the length of the string
+print(message.split(',')) # Splits the string into a list at each comma
+print(message.replace("World", "Python")) # Replaces "World" with "Python"
+print(message.upper()) # Converts to uppercase
---
+++ numeric.py
@@ -0,0 +1,13 @@
+#Numeric Data
+
+num = 3
+
+print(3)
+
+num2 = 3.14
+
+print(3.14)
+
+print(type(num))
+
+print(type(num2))
