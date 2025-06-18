0df5b0ac1984642e934fcd4f4d1288c4b3a02c1d
EatenCaesar-ZA <irshadmoola@gmail.com>
2025-06-18 22:11:10 +0200

    Create operatorwithstrings.py

---
+++ operatorwithstrings.py
@@ -0,0 +1,9 @@
+#Operators with Strings
+
+strng1 = "Hello"
+strng2 = "World"
+
+print(strng1 + strng2)
+print(strng1 * 3)
+print(strng1 + " " + strng2)  # Concatenate with a space in between 
+print(strng1 +" "+ strng2 +" " + strng2)
