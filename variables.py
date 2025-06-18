2b13c6c907bc89a09c4ac2c7af813c4fd95ab341
EatenCaesar-ZA <irshadmoola@gmail.com>
2025-06-18 21:27:51 +0200

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,11 @@
+#Variables
+
+# Valid Variables
+my_variable = 10     # rules for variable,must start with a letter or underscore, can contain letters and underscores
+totalCount = 5
+userName = "John"
+
+# Invalid Variables
+1stVariable = 10   # cannot start with a number
+total-count = 5    # cannot contain hyphens
+user name = "John"  # cannot contain spaces
